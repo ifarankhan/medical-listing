1ca659d3327f0f615656ff4c6e4c564ac844208d
RT <raoraafe@gmail.com>
2024-11-15 19:49:30 +0500

    TASK\DIV-91: Trial period without credit card information.

--- resources/views/listing/index.blade.php
+++ resources/views/listing/index.blade.php
@@ -1,3 +1,4 @@
+@php use App\Models\Listing; @endphp
 {{-- resources/views/listings/index.blade.php --}}
 @extends('layout')
 
@@ -33,47 +34,52 @@
                         <table>
                             <thead>
                             <tr>
-    {{--                            <th class="images">images</th>--}}
+                                {{--                            <th class="images">images</th>--}}
                                 <th class="details">details</th>
-    {{--                            <th class="price">price</th>--}}
+                                {{--                            <th class="price">price</th>--}}
                                 <th class="status">status</th>
                                 <th class="action">action</th>
                             </tr>
                             </thead>
                             <tbody>
-                                @foreach($listings as $listing)
-                                    <tr>
-            {{--                            <td class="images">--}}
-            {{--                                <img src="{{ asset('frontend/images/listing_1.jpg') }}" alt="property" class="img-fluid w-100">--}}
-            {{--                            </td>--}}
+                            @foreach($listings as $listing)
+                                <tr>
+                                    {{--                            <td class="images">--}}
+                                    {{--                                <img src="{{ asset('frontend/images/listing_1.jpg') }}" alt="property" class="img-fluid w-100">--}}
+                                    {{--                            </td>--}}
 
-                                        <td class="details">
-                                            <a class="item_title" href="{{ route('listing.edit', $listing ) }}">{{ $listing->business_name }}</a>
-                                            <p>Posting date: {{ $listing->created_at->format('F j, Y') }}</p>
-                                            {{--<span>
-                                                    <i class="fas fa-star"></i>
-                                                    <i class="fas fa-star"></i>
-                                                    <i class="fas fa-star"></i>
-                                                    <i class="fas fa-star"></i>
-                                                    <i class="fas fa-star"></i>
-                                                    <b>(24 Reviews)</b>
-                                                </span>--}}
-                                        </td>
+                                    <td class="details">
+                                        <a class="item_title"
+                                           href="{{ route('listing.edit', $listing ) }}">{{ $listing->business_name }}</a>
+                                        <p>Posting date: {{ $listing->created_at->format('F j, Y') }}</p>
+                                        {{--<span>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <b>(24 Reviews)</b>
+                                            </span>--}}
+                                    </td>
 
-                                        <td class="status">
-                                            <span class="sold">{{ $listing->getFormattedStatus() }}</span>
-                                            @if ($listing->user && !$listing->user->has_trial_ended)
-                                                <p class="trial-end-date mt-1 text-danger">
-                                                    Trial Ends: <b>{{ $listing->user->formatted_trial_end_date }}</b>
-                                                </p>
-                                            @endif
-                                        </td>
-                                        <td class="action">
-                                            <a href="{{ route('listing.edit', $listing) }}"><i class="far fa-pen"></i> Edit</a>
-                                            <a href="{{ route('listing.delete', $listing) }}"><i class="far fa-trash"></i> Delete</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                    <td class="status">
+                                        <span class="sold">{{ $listing->getFormattedStatus() }}</span>
+                                        @if (
+                                                $listing->user?->has_trial_ended === false
+                                                && $listing->listing_status === Listing::STATUS_ACTIVE_TRIAL
+                                            )
+                                            <p class="trial-end-date mt-1 text-danger">
+                                                Trial Ends: <b>{{ $listing->user->formatted_trial_end_date }}</b>
+                                            </p>
+                                        @endif
+                                    </td>
+                                    <td class="action">
+                                        <a href="{{ route('listing.edit', $listing) }}"><i class="far fa-pen"></i> Edit</a>
+                                        <a href="{{ route('listing.delete', $listing) }}"><i class="far fa-trash"></i>
+                                            Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
