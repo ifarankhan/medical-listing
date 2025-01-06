6740bd00d577bf144d18af4e6975b87d4ed1fe84
Raafe Tahir <raoraafe@gmail.com>
2025-01-06 22:02:56 +0500

    HOTFIX\BUG: Fixed! Get in touch email broken due to mailgun problem.

--- app/Http/Controllers/GetInTouchController.php
+++ app/Http/Controllers/GetInTouchController.php
@@ -11,23 +11,31 @@ class GetInTouchController extends Controller
 {
     public function sendEmail(Request $request): JsonResponse
     {
-        // Validate the email field
-        $request->validate([
-            'email' => 'required|email',
-        ]);
-        // Prepare the data for the email.
-        $email = $request->input('email');
-        $toEmail = env('INO_EMAIL', 'info@diverrx.com');
+        try {
+            // Validate the email field
+            $request->validate([
+                'email' => 'required|email',
+            ]);
+            // Prepare the data for the email.
+            $email = $request->input('email');
+            $toEmail = env('INO_EMAIL', 'info@diverrx.com');
 
-        Mail::to($toEmail)
-            ->send(new GetInTouchMail([
-                'fromEmail' => 'no-reply@diverrx.com',
-                'email' => $email,
-            ]));
-        // Return a success response
-        return response()->json([
-            'success' => true,
-            'message' => 'Thank you for getting in touch!'
-        ]);
+            Mail::to($toEmail)
+                ->send(new GetInTouchMail([
+                    'fromEmail' => 'no-reply@diverrx.com',
+                    'email' => $email,
+                ]));
+            // Return a success response
+            return response()->json([
+                'success' => true,
+                'message' => 'Thank you for getting in touch!'
+            ]);
+        } catch (\Exception $ex) {
+            // Return a failure response
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to send email. Please try again later.'
+            ], 500);
+        }
     }
 }
