1725900ee8b70c8e79cb0db14cf90f2edeb24796
Raafe Tahir <raoraafe@gmail.com>
2025-02-16 23:13:36 +0500

    TASK\DIV-74: Reviews Page in Dashboard

--- resources/views/dashboard/reviews/grid.blade.php
+++ resources/views/dashboard/reviews/grid.blade.php
@@ -9,9 +9,15 @@
             <h2 class="dashboard_title">Reviews</h2>
 
             <div class="dashboard_reviews wow fadeInUp" data-wow-duration="1.5s">
-                <x-review-section
-                    :reviews="$reviews"
-                    :showCount="false"/>
+                @if ($reviews !== null && $reviews->isNotEmpty())
+                    <x-review-section
+                        :reviews="$reviews"
+                        :showCount="false"/>
+                @else
+                    <p style="font-size: 1.2rem; color: #555; text-align: center; margin-top: 20px; padding: 15px; border: 1px solid #ccc; border-radius: 5px; background-color: #f9f9f9;">
+                        There are currently no reviews for your business profile.
+                    </p>
+                @endif
             </div>
         </div>
     </section>
